9dfc9cbe8373f6f74d3d586cdfc0dcbcbc6b8961
“soudanieldias” <danieldias.b99@hotmail.com>
2022-06-11 16:04:08 -0300

    feat: import assets and load sky image

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -7,16 +7,22 @@ export default class Demo extends Phaser.Scene {
 
   preload() { // Preload of elements used in game (images, songs, etc)
     // Images
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('form', 'assets/forms.png');
+    this.load.image('sky', '../public/assets/sky.png');
+    this.load.image('ground', '../public/assets/platform.png');
+    this.load.image('star', '../public/assets/star.png');
+    this.load.image('form', '../public/assets/forms.png');
 
-    //Audios/Songs/Effects
+    // Sprites
+    this.load.spritesheet('dude', '../public/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+
+    // Audios - Songs - Effects
     this.load.audio('backgroundSong', ['../public/assets/audio/backgroundSong.mp3']);
+  
   }
 
   create() { // Assignment & Bind of elements to Scene
+    this.add.image(400, 300, 'sky');
+
     const backgroundSong = this.sound.add('backgroundSong', {volume: 0.3});
     backgroundSong.play();
   }
