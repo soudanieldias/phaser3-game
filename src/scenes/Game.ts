400946e145ddffad6a048d4c8f4fd060435a1c4f
“soudanieldias” <danieldias.b99@hotmail.com>
2022-06-11 15:36:54 -0300

    feat: set initial files for new game

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,19 +6,14 @@ export default class Demo extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
+  }
+
+  update () {
+
   }
 }
