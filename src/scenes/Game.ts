a435d85b1e97dce5eb5e14959cc77e51ba6ef5cc
“soudanieldias” <danieldias.b99@hotmail.com>
2022-06-12 04:58:07 -0300

    feat: add cursors logic at update function

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -119,6 +119,22 @@ export default class Demo extends Phaser.Scene {
   }
 
   public update () {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.anims.play('left', true);
 
+    } else if (this.cursors.right.isDown) {
+       this.player.setVelocityX(160);
+      this.player.anims.play('right', true);
+
+    } else {
+       this.player.setVelocityX(0);
+      this.player.anims.play('turn');
+
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
   }
 }
