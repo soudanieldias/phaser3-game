68673ddefc535f8083d14c8e88cab7a35b160457
“soudanieldias” <danieldias.b99@hotmail.com>
2022-06-13 22:39:43 -0300

    feat: add Main scene to scenes

---
+++ src/scenes/Main.ts
@@ -0,0 +1,32 @@
+import Phaser from 'phaser';
+
+export default class Main extends Phaser.Scene {
+  constructor() {
+    super('MainScene');
+  }
+
+  public preload () { // Preload of elements used in game (images, songs, etc)
+    // Images
+
+    // Sprites
+
+    // Audios - Songs - Effects
+  }
+
+  public create () { // Assignment & Bind of elements to Scene
+    // Images
+    
+    // Elements
+
+    // Overlaps
+  
+    // Animations
+
+    // Audios - Songs - Effects
+
+    // Events
+  }
+
+  public update () {
+  }
+}
