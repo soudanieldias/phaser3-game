be8267da25480d8363909c62f44bf7a7ec467fde
“soudanieldias” <danieldias.b99@hotmail.com>
2022-06-13 22:31:14 -0300

    feat: add pause scene for game

---
+++ src/scenes/Pause.ts
@@ -0,0 +1,43 @@
+import Phaser from 'phaser';
+
+export default class Demo extends Phaser.Scene {
+  constructor() {
+    super('PauseScene');
+  }
+
+  private resumeButton: any;
+
+  public preload () { // Preload of elements used in game (images, songs, etc)
+    // Images
+
+    // Sprites
+
+    // Audios - Songs - Effects
+
+  }
+
+  public create () { // Assignment & Bind of elements to Scene
+    // Images
+    
+    // Elements
+    // Element - resumeButton
+    this.resumeButton = this.add.text(300, 400, 'Resume', { fontSize: '60px', color: '#000' })
+    .setInteractive({ useHandCursor: true })
+    .on('pointerdown', () => this.resumeGame());
+
+    // Overlaps
+  
+    // Animations
+
+    // Audios - Songs - Effects
+
+    // Events
+  }
+
+  public update () {
+  }
+
+  private resumeGame () {
+    this.scene.switch('GameScene');
+  }
+}
