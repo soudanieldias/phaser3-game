a0d064cb46d59e398fa087dc505bf55a868354aa
“soudanieldias” <danieldias.b99@hotmail.com>
2022-06-13 22:32:11 -0300

    feat: import PauseScene to scenet object on index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import Phaser from 'phaser';
 import config from './config';
 import GameScene from './scenes/Game';
+import PauseScene from './scenes/Pause';
 
 new Phaser.Game(
   Object.assign(config, {
-    scene: [GameScene]
+    scene: [GameScene, PauseScene]
   })
 );
